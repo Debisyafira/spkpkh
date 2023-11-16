a739e0c61ceee0604b59265ccc98eb2b35014561
alvnfaiz <alvin.faiz.10@gmail.com>
2023-11-16 07:52:40 +0700

    add excel pkh

--- app/Http/Controllers/PkhController.php
+++ app/Http/Controllers/PkhController.php
@@ -6,24 +6,23 @@ use App\Models\Calon_pkh;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redirect;
-
-
+use Maatwebsite\Excel\Facades\Excel;
 
 class PkhController extends Controller
 {
     public function index()
     {
         $data = Calon_pkh::get();
+
         // dd($data);
         return view('dashboards.admins.pkh.index', compact('data'));
     }
-
-
 
     public function create()
     {
         return view('dashboards.admins.pkh.add');
     }
+
     public function store(Request $request)
     {
         $input = Calon_pkh::insert([
@@ -37,26 +36,29 @@ class PkhController extends Controller
         } else {
             echo "<script>
             alert('Data gagal diinput, masukkan kembali data dengan bener');
-            window.location = '" . route('admin.pkh') . "';
+            window.location = '".route('admin.pkh')."';
             </script>";
         }
     }
+
     public function edit(Request $req)
     {
         $data = DB::table('calon_pkhs')
             ->where('id', $req->id)
             ->first();
+
         // dd($data);
         return view('dashboards.admins.pkh.edit', compact('data'));
     }
+
     public function update(Request $request)
     {
-        $update =  DB::table('calon_pkhs')
+        $update = DB::table('calon_pkhs')
             ->where('id', $request->id)
             ->update([
                 'nik' => $request->nik,
                 'nama' => $request->nama,
-                'alamat' => $request->alamat
+                'alamat' => $request->alamat,
             ]);
         if ($update) {
             return redirect(route('admin.pkh'))->with('pesan', 'Data PKH berhasil disimpan');
@@ -67,14 +69,46 @@ class PkhController extends Controller
                 </script>";
         }
     }
+
     public function destroy(Request $req)
     {
-
-        $data = Calon_pkh::findOrFail($req->id); //ganti dengan model dan nama tabel yang sesuai
+        $data = Calon_pkh::findOrFail($req->id); // ganti dengan model dan nama tabel yang sesuai
         $data->delete();
+
         return json_encode([
-            'status' => 'success'
+            'status' => 'success',
         ]);
         // return redirect()->route('admin.pkh')->with('success', 'Data PKH berhasil dihapus');
+    }
+
+    public function importExcel(Request $request)
+    {
+        if ($request->hasFile('excel_file')) {
+            $file = $request->file('excel_file');
+
+            // Validate the file
+            $validated = $request->validate([
+                'excel_file' => 'required|mimes:xlsx,xls,csv',
+            ]);
+
+            // Extract data from the file start from row 2
+            $data = Excel::toCollection([], $file)[0];
+
+            // Loop through each row and add to calon_pkhs
+            foreach ($data as $row) {
+                if (!isset($row[0])) {
+                    return null;
+                }
+                Calon_pkh::create([
+                    'nik' => $row[0],
+                    'nama' => $row[1],
+                    'alamat' => $row[2],
+                ]);
+            }
+
+            return redirect()->route('admin.pkh')->with('success', 'Data PKH imported successfully');
+        } else {
+            return redirect()->route('admin.pkh')->with('error', 'Please select a file to import');
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\CriteriaController;
 use App\Http\Controllers\HomeController;
@@ -8,9 +7,8 @@ use App\Http\Controllers\KriteriaTerbobotController;
 use App\Http\Controllers\PkhController;
 use App\Http\Controllers\RatioCriteriaController;
 use App\Http\Controllers\SubKriteriaController;
-use App\Http\Controllers\UserController;
-use App\Models\kriteria_terbobot;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,34 +32,35 @@ Auth::routes();
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-// ADMIN 
+// ADMIN
 // Route::group(['prefix' => 'admin', 'middleware' => ['isAdmin', 'auth', 'PreventBackHistory']], function () {
 Route::group(['prefix' => 'admin', 'middleware' => ['auth']], function () {
     Route::get('dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
     // Route::get('profile', [AdminController::class, 'profile'])->name('admin.profile');
     // Route::get('user', [AdminController::class, 'user'])->name('admin.user');
 
-    //PKH
+    // PKH
     Route::get('pkh', [PkhController::class, 'index'])->name('admin.pkh');
     Route::get('pkh/create', [PkhController::class, 'create'])->name('admin.pkh.create');
     Route::post('pkh/simpan', [PkhController::class, 'store'])->name('admin.pkh.simpan');
     Route::get('pkh/edit', [PkhController::class, 'edit'])->name('admin.pkh.edit');
     Route::put('pkh/update', [PkhController::class, 'update'])->name('admin.pkh.update');
     Route::delete('pkh/delete', [PkhController::class, 'destroy'])->name('admin.pkh.delete');
+    Route::post('pkh/excel', [PkhController::class, 'importExcel'])->name('admin.pkh.excel');
 
-    //Kriteria
+    // Kriteria
     Route::get('/criteria', [CriteriaController::class, 'index'])->name('admin.criteria');
     Route::post('/addCriteria', [CriteriaController::class, 'store'])->name('admin.addCriteria');
     Route::get('/deleteCriteria/{criteria}', [CriteriaController::class, 'destroy'])->name('admin.deleteCriteria');
 
-    //Ratio Kriteria
+    // Ratio Kriteria
     Route::get('/ratioCriteria', [RatioCriteriaController::class, 'index'])->name('admin.ratioCriteria');
     Route::post('/addRatioCriteria', [CriteriaController::class, 'storeRatio'])->name('admin.addRatioCriteria');
     Route::post('/massRatioCriteria', [CriteriaController::class, 'massUpdate'])->name('admin.massRatioCriteria');
     Route::get('/deleteRatioCriteria/{v_id}/{h_id}', [RatioCriteriaController::class, 'destroy'])->name('admin.deleteRatioCriteria');
-    //End Kriteria
+    // End Kriteria
 
-    //Kriteria terbobot
+    // Kriteria terbobot
     Route::get('kriteria_terbobot', [KriteriaTerbobotController::class, 'index'])->name('admin.kriteria_terbobot');
     Route::get('kriteria_terbobot/create', [KriteriaTerbobotController::class, 'create'])->name('admin.kriteria_terbobot.create');
     Route::post('kriteria_terbobot/simpan', [KriteriaTerbobotController::class, 'store'])->name('admin.kriteria_terbobot.simpan');
@@ -69,6 +68,6 @@ Route::group(['prefix' => 'admin', 'middleware' => ['auth']], function () {
     Route::put('kriteria_terbobot/update', [KriteriaTerbobotController::class, 'update'])->name('admin.kriteria_terbobot.update');
     Route::delete('kriteria_terbobot/delete', [KriteriaTerbobotController::class, 'destroy'])->name('admin.kriteria_terbobot.delete');
 
-    //data sub kriteria
+    // data sub kriteria
     // Route::get('/subkriteria/{criteria}/create', SubKriteriaController::class)->name('subkriteria.create');
 });
