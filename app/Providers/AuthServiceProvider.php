35ce2bb03527c0fab9107f07a6498695c4b75b39
Andi-IM <andi.irhamm@gmail.com>
2023-12-06 15:17:35 +0700

    adding user log dependency and create gates

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Enums\RoleEnum;
+use Illuminate\Auth\Access\Response;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -24,7 +26,16 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+        Gate::define('isAdmin', function ($user) {
+            return $user->role == RoleEnum::ADMIN ? Response::allow() : Response::deny('You must be an admin to access this feature');
+        });
 
-        //
+        Gate::define('isOpt', function ($user) {
+            return $user->role == RoleEnum::OPT ? Response::allow() : Response::deny('You must be an operator to access this feature');
+        });
+
+        Gate::define('isUser', function ($user) {
+            return $user->role == RoleEnum::USER ? Response::allow() : Response::deny('You must be an user to access this feature');
+        });
     }
 }
